e060be6b98be285bcb627e34567d346918551158
Amanpreet Singh <amanpreetsinghlubana@gmail.com>
2024-08-24 17:57:05 +0530

    Added Links to pages

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,3 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Products() {
-  return <h1>Products</h1>;
+  return (
+    <>
+      <h1>Products</h1>
+      <p>
+        Go to <Link to="/">homepage</Link>
+      </p>
+    </>
+  );
 }
