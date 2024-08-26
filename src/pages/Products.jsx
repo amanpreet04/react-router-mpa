8eb707515bf42e484471c17f7738439d4dd64cf0
Amanpreet Singh <amanpreetsinghlubana@gmail.com>
2024-08-26 23:08:38 +0530

    building dynamic routes

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,12 +1,30 @@
 import { Link } from "react-router-dom";
+const PRODUCTS = [
+  {
+    id: "p1",
+    title: "Product 1",
+  },
+  {
+    id: "p2",
+    title: "Product 2",
+  },
+  {
+    id: "p3",
+    title: "Product 3",
+  },
+];
 
 export default function Products() {
   return (
     <>
       <h1>Products</h1>
-      <p>
-        Go to <Link to="/">homepage</Link>
-      </p>
+      <ul>
+        {PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Link to={`/products/${product.id}`}>{product.title}</Link>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
